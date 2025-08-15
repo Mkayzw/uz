696fc4d9a2d12b1ddb67cf515c699a3fa2136ad7
Mkayzw <mkaytech1@gmail.com>
2025-08-15 18:15:04 +0200

    ichoo

--- supabase/migrations/20250123_add_gender_to_profiles.sql
+++
@@ -1,5 +0,0 @@
--- Add gender field to profiles table
-ALTER TABLE profiles 
-ADD COLUMN gender VARCHAR(20) CHECK (gender IN ('male', 'female', 'other', 'prefer_not_to_say'));
-
-COMMENT ON COLUMN profiles.gender IS 'Gender information for tenant profiles displayed to agents';
--- supabase/migrations/20250811_add_application_property_fk.sql
+++
@@ -1,5 +0,0 @@
--- Create application<->property relationship
-ALTER TABLE applications
-ADD COLUMN property_id UUID REFERENCES properties(id);
-
-CREATE INDEX idx_applications_property_id ON applications(property_id);
